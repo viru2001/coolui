a34671bb8ca50ef48adc2e275e74a4e0270c19a7
AmitMaregude <amitmaregude.it.com>
2022-10-02 23:05:04 +0530

    fix navbar-bug

--- js/docs-nav.js
+++ js/docs-nav.js
@@ -1,18 +1,21 @@
 const hamburgMenuBtn = document.querySelector("#btn-hamburger");
 const hamburgMenu = document.querySelector("#nav-menu-hamburger");
-
 const compNavLinks = document.querySelectorAll(".nav-link-item");
 
-// toggle display none class
-hamburgMenuBtn.addEventListener("click", () => {
-  const classes = hamburgMenu.classList;
-  classes.toggle("d-none");
-});
+let viewportWidth = window.innerWidth;
 
-// hide component navigation after selecting link from nav menu
-compNavLinks.forEach(link => {
-  link.addEventListener("click", () => {
+if (viewportWidth <= 885) {
+  // toggle display none class
+  hamburgMenuBtn.addEventListener("click", () => {
     const classes = hamburgMenu.classList;
     classes.toggle("d-none");
   });
-});
+
+  // hide component navigation after selecting link from nav menu
+  compNavLinks.forEach(link => {
+    link.addEventListener("click", () => {
+      const classes = hamburgMenu.classList;
+      classes.toggle("d-none");
+    });
+  });
+}
